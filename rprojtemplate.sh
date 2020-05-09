7aa712a18326dabf2c9e97f2616b7f9b3a1a3478
Luis Puerto <luis@luispuerto.net>
2020-05-09 21:47:23 +0300

    Add script to copy template to current folder

---
+++ rprojtemplate.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# copy the template structure to the current folders
+rprojtemplate (){ 
+
+  DIR="/Users/lpuerto/Code/01 Templates/R-Proj-Template"
+  cp -r $DIR/Template/ . 
+
+}
